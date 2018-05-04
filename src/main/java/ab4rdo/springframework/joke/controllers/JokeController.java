68b400a6156242f5742c29a4e7d7baedda415db9
Ab4rdo <fryderyk97@gmail.com>
2018-05-04 16:29:48 +0200

    added Readme and implementatino of the project

---
+++ src/main/java/ab4rdo/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,29 @@
+package ab4rdo.springframework.joke.controllers;
+
+import ab4rdo.springframework.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Fred on 2018-05-04.
+ */
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
---
+++ src/main/java/ab4rdo/springframework/joke/services/JokeService.java
@@ -0,0 +1,9 @@
+package ab4rdo.springframework.joke.services;
+
+/**
+ * Created by Fred on 2018-05-04.
+ */
+public interface JokeService {
+
+    String getJoke();
+}
